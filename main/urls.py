1394406c5b37a7881e511580e6566bf0e974aaf0
syarna <112332315+syarna@users.noreply.github.com>
2023-09-20 13:17:05 +0700

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,3 +1,4 @@
+from main.views import register #sesuaikan dengan nama fungsi yang dibuat
 from django.urls import path
 from main.views import show_main
 from django.urls import path, include
@@ -10,4 +11,7 @@ urlpatterns = [
     path('create-product', create_product, name='create_product'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
+    path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<int:id>/', show_json_by_id, name='show_json_by_id'),
+    path('register/', register, name='register'),
 ]
