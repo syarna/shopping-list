be243a68c98b24bfce2a46fc7ae2a8f0a9f58a9c
syarna <112332315+syarna@users.noreply.github.com>
2023-09-20 13:21:22 +0700

    Update views.py

--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from main.forms import ProductForm
@@ -9,6 +10,19 @@ from django.shortcuts import redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('main:show_main')
+        else:
+            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
+    context = {}
+    return render(request, 'login.html', context)
+    
 def register(request):
     form = UserCreationForm()
 
